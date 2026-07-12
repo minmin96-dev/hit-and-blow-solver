17f18f0c12247085c48edd9f24878c6646e7639f
minmin <302704099+minmin96-dev@users.noreply.github.com>
2026-07-12 18:16:16 +0900

    feat: Sprint7-A UI整理

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const gameState = {
 
     hit: 0,
     blow: 0,
-    showCandidates: false
+    showCandidates: false,
+    showHeatmap: false
 };
 
 render();
@@ -17,6 +18,7 @@ function render() {
     renderColorPalette();
     renderHistory();
     renderCandidateList();
+    renderHeatmap();
 }
 
 function renderCandidateCount() {
@@ -215,3 +217,97 @@ function renderCandidateList() {
         }
     );
 }
+
+function calculateHeatmap() {
+    const result = [];
+
+    for (let position = 0; position < 4; position++) {
+        const count = {};
+        for (const color of COLORS) {
+            count[color.id] = 0;
+        }
+
+        for (const candidate of gameState.candidates) {
+            const colorId =
+                candidate[position];
+            count[colorId]++;
+        }
+
+        const total =
+            gameState.candidates.length;
+
+        const positionResult =
+            COLORS.map(color => {
+                return {
+                    colorId: color.id,
+                    percentage:
+                        total === 0
+                            ? 0
+                            : Math.round(
+                                count[color.id]
+                                / total
+                                * 100
+                            )
+                };
+            });
+        result.push(positionResult);
+    }
+    return result;
+}
+
+function renderHeatmap() {
+    const area =
+        document.getElementById("heatmap-area");
+
+    if (gameState.showHeatmap) {
+        area.classList.remove("hidden");
+    } else {
+        area.classList.add("hidden");
+        return;
+    }
+
+    const list =
+        document.getElementById("heatmap-list");
+
+    list.innerHTML = "";
+
+    const heatmap =
+        calculateHeatmap();
+
+    heatmap.forEach(
+        (positionData, index) => {
+
+            const title =
+                document.createElement("h3");
+
+            title.textContent = `${index + 1}個目`;
+            list.appendChild(title);
+
+            positionData.forEach(
+                data => {
+                    const div =
+                        document.createElement("div");
+
+                    div.textContent = `${COLORS[data.colorId].name} : ${data.percentage}%`;
+                    list.appendChild(div);
+                }
+            );
+        }
+    );
+}
+
+document
+    .getElementById("toggle-heatmap-button")
+    .onclick = () => {
+        gameState.showHeatmap =
+            !gameState.showHeatmap;
+
+        document
+            .getElementById("toggle-heatmap-button")
+            .textContent =
+                gameState.showHeatmap
+                    ? "ヒートマップを隠す"
+                    : "ヒートマップを見る";
+        render();
+    };
+
